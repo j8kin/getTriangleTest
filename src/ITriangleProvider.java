f7a3be5856b98ce97fa066c7ad479e6f57b02403
j8kin <emk161278@gmail.com>
2018-08-24 19:46:16 +0300

    Update Provider for testing purposes to be able to set value which getTriangle returns and need to be verified.

--- src/ITriangleProvider.java
+++ src/ITriangleProvider.java
@@ -1,35 +1,20 @@
 public final class ITriangleProvider {
+    // corners is created for testing purposes set it test
+    private static ITriangle corners;
+
+    /**
+     * set Triangle method. Created for test purpose
+     * @param inCorners corner to be returned by getTriangle
+     */
+    public static void setTriangle(ITriangle inCorners) {
+        corners = inCorners;
+    }
+
+    /**
+     * This method returns points on The Cartesian plane which
+     * @return Cartesian plane which represents Right Triangle
+     */
     public static ITriangle getTriangle() {
-        return new ITriangle() {
-            @Override
-            public int getX1() {
-                return 0;
-            }
-
-            @Override
-            public int getX2() {
-                return 0;
-            }
-
-            @Override
-            public int getX3() {
-                return 0;
-            }
-
-            @Override
-            public int getY1() {
-                return 0;
-            }
-
-            @Override
-            public int getY2() {
-                return 0;
-            }
-
-            @Override
-            public int getY3() {
-                return 0;
-            }
-        };
+        return corners;
     }
 }
